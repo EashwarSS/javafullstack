5d92edebecec15af5ca4fb0ef23083ce51071457
EashwarSS <eashwarlucky@gmail.com>
2026-01-08 20:42:47 +0530

    sec5

--- section4/eazystore-ui/src/App.jsx
+++ section4/eazystore-ui/src/App.jsx
@@ -1,8 +1,11 @@
 import Header from "./components/Header";
-// import ClassHeader from "./components/ClassHeader";
+import ClassHeader from "./components/ClassHeader";
 import Footer from "./components/footer/Footer";
 import React from "react";
 import Home from "./components/Home";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "./custom.scss";
 
 function App() {
   return (
